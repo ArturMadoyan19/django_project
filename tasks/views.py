64322ff8a12ae5178a7795eda2ab81eff0f5edd1
Artur Madoyan <madoyan017@icloud.com>
2025-04-03 16:58:17 +0400

    verjnakan

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 def task_list(request):
@@ -7,5 +7,17 @@ def task_list(request):
         if title:
             Task.objects.create(title=title)
         return redirect('task_list')
+    
     tasks = Task.objects.all()
     return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
