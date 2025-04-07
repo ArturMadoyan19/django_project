d0e52a8047f349cc084fe477b2d1f0f401060370
ArturMadoyan <madoyan017@icloud.com>
2025-04-07 23:54:37 +0400

    login

--- application/urls.py
+++ application/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import info_view
+from .views import login_view, info_view
 
 urlpatterns = [
+    path('', login_view, name='login_view'),
     path('', info_view, name='info_view'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Login
 
 def info_view(request):
     return render(request, "application/index.html")
+
+def login_view(request):
+    login = ''
+    password = ''
+    success = False
+    if request.method == 'POST':
+        login = request.POST.get('login', '')
+        password = request.POST.get('password', '')
+        if login == "Artur" and password == "Madoyan":
+            success = True
+    return render(request, 'application/index.html',
+                    {'login_value': login, 
+                    'password_value': password,
+                    'success':success})
+        
+
